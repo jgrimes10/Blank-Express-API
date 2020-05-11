6f4a553489dcfb9010d7a36d83890eff94b1f761
jgrimes@relias.com <jgrimes@relias.com>
2020-05-11 10:38:47 -0400

    Fix failing test

--- test/users.test.js
+++ test/users.test.js
@@ -86,7 +86,7 @@ describe('Students', () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('array');
-          res.body.length.should.be.eql(0);
+          res.body.length.should.be.eql(2);
           done();
         });
     });
